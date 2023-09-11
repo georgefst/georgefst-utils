a6e3b199fc0a1ab9fadd1b129118c42e6559d4be
George Thomas <georgefsthomas@gmail.com>
2023-09-11 10:24:53 +0100

    Close socket on exit

--- streamly-udp/Util/Streamly/UDP.hs
+++ streamly-udp/Util/Streamly/UDP.hs
@@ -8,14 +8,16 @@ import Network.Socket (
     SockAddr (SockAddrInet),
     SocketType (Datagram),
     bind,
+    close,
     defaultProtocol,
     socket,
  )
 import Network.Socket.ByteString (recv)
 import Streamly.Data.Stream.Prelude qualified as S
-import Util.Streamly qualified as S
 
 stream :: (MonadIO n) => PortNumber -> S.Stream n BSL.ByteString
-stream port = S.morphInner liftIO $ S.withInit
-    (socket AF_INET Datagram defaultProtocol >>= \s -> bind s (SockAddrInet port 0) >> pure s)
-    \sock -> S.repeatM $ BSL.fromStrict <$> recv sock 4096
+stream port = S.morphInner liftIO $
+    S.bracket
+        (socket AF_INET Datagram defaultProtocol >>= \s -> bind s (SockAddrInet port 0) >> pure s)
+        close
+        \sock -> S.repeatM $ BSL.fromStrict <$> recv sock 4096
