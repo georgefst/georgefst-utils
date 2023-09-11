c0e6326d18ee33389293a1dbd2126ca40fd0d13d
George Thomas <georgefsthomas@gmail.com>
2023-09-11 10:10:09 +0100

    Comments

--- streamly-utils/Util/Streamly.hs
+++ streamly-utils/Util/Streamly.hs
@@ -6,11 +6,13 @@ import Streamly.Data.Stream.Prelude qualified as S
 import Streamly.Data.StreamK qualified as SK
 import Streamly.Internal.Data.Stream.StreamK qualified as SK
 
+-- TODO look at `S.bracket` implementation for better way to do this?
 withInit :: (Monad m) => m t -> (t -> S.Stream m a) -> S.Stream m a
 withInit init_ stream = SK.toStream $ SK.unCross do
     m <- SK.mkCross $ SK.fromStream $ S.fromEffect init_
     SK.mkCross . SK.fromStream $ stream m
 
+-- TODO ask upstream about easier approaches?
 fromEmitter :: (S.MonadAsync m) => ((a -> m ()) -> m ()) -> S.Stream m a
 fromEmitter f = withInit (liftIO newEmptyMVar) \m ->
     (S.catMaybes . S.parList id)
