3073639bf2bef59fc8f0517106549e28da8822b4
George Thomas <georgefsthomas@gmail.com>
2023-12-30 12:26:43 +0000

    Remove GPIO monitoring lines from API entirely
    Now that we don't handle our own debouncing, there's really nothing interesting there.

--- gpio-utils/Util/GPIO/Mon.hs
+++ gpio-utils/Util/GPIO/Mon.hs
@@ -3,7 +3,7 @@ module Util.GPIO.Mon (mon) where
 import Control.Monad (forever)
 import Control.Monad.IO.Class (MonadIO (..))
 import Data.ByteString (ByteString)
-import Data.Text (Text, pack)
+import Data.Text (pack)
 import Data.Text.Encoding (encodeUtf8)
 import Data.Time (NominalDiffTime, nominalDiffTimeToSeconds)
 import RawFilePath (CreatePipe (CreatePipe), proc, processStdout, setStdout, startProcess)
@@ -11,15 +11,15 @@ import System.IO (hGetLine)
 
 -- TODO "debounced" events are very often just the button being released
 -- if we had a way to distinguish (which I think we would if not for dodgy wires), we could set the threshold much lower
-mon :: (MonadIO m) => ByteString -> (Text -> m ()) -> NominalDiffTime -> Int -> m ()
-mon gpioChip putLine debounce pin = do
+mon :: (MonadIO m) => ByteString -> m () -> NominalDiffTime -> Int -> m ()
+mon gpioChip action debounce pin = do
     p <-
         liftIO . startProcess $
             proc "gpiomon" ["--edges", "falling", "--chip", gpioChip, "--debounce-period", debounceString, showBS pin]
                 `setStdout` CreatePipe
     forever do
-        line <- liftIO . hGetLine $ processStdout p
-        putLine (pack line)
+        _line <- liftIO . hGetLine $ processStdout p
+        action
   where
     debounceString = (<> "us") . showBS @Integer . round . (* 1_000_000) $ nominalDiffTimeToSeconds debounce
     showBS :: (Show a) => a -> ByteString
--- streamly-gpio/Util/Streamly/GPIO.hs
+++ streamly-gpio/Util/Streamly/GPIO.hs
@@ -1,7 +1,6 @@
 module Util.Streamly.GPIO where
 
 import Data.ByteString (ByteString)
-import Data.Text (Text)
 import Data.Time (NominalDiffTime)
 import Streamly.Data.Stream.Prelude qualified as S
 import Util.GPIO qualified as GPIO
@@ -13,10 +12,6 @@ data Opts = Opts
     , debounce :: NominalDiffTime
     }
 
-data Item
-    = Event
-    | OutLine {line :: Text}
-
-stream :: (S.MonadAsync m) => Opts -> S.Stream m Item
+stream :: (S.MonadAsync m) => Opts -> S.Stream m ()
 stream Opts{..} = S.fromEmitter \f ->
-    GPIO.mon chip (\line -> f OutLine{..} >> f Event) debounce pin
+    GPIO.mon chip (f ()) debounce pin
